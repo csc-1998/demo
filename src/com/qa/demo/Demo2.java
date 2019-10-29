848b42864c9885321b0eb2c72283127b3780d2dc
csc-1998 <2557180266@qq.com>
2019-10-29 10:36:00 +0800

    abc

--- src/com/qa/demo/Demo2.java
+++ src/com/qa/demo/Demo2.java
@@ -1,9 +1,10 @@
-package com.qa.demo;
+﻿package com.qa.demo;
 
 public class Demo2 {
 public static void main(String[] args) {
 	System.out.println("nihoa");
 	//这是我的修改
+	System.out.println("nishi");
 }
 	
 }
